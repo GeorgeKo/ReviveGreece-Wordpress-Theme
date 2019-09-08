238774aaf677fb45af5a9a8cd261333b2f705dcc
GeorgeKo <16765294+GeorgeKo@users.noreply.github.com>
2019-09-08 23:05:50 +0300

    Add custom Taxonomies

--- functions.php
+++ functions.php
@@ -383,3 +383,34 @@ function Books_save_ISBN_data( $post_id ) {
 
 add_action('add_meta_boxes', 'Books_ISBN_add_meta_box');
 add_action('save_post', 'Books_save_ISBN_data');
+
+
+/************ Taxonomies for Books ***********/
+add_action( 'init', 'books_ _custom_taxonomy', 0 );
+ 
+//create a custom taxonomy name it "type" for your posts
+function crunchify_create_deals_custom_taxonomy() {
+ 
+  $labels = array(
+    'name' => _x( 'Types', 'taxonomy general name' ),
+    'singular_name' => _x( 'Type', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Types' ),
+    'all_items' => __( 'All Types' ),
+    'parent_item' => __( 'Parent Type' ),
+    'parent_item_colon' => __( 'Parent Type:' ),
+    'edit_item' => __( 'Edit Type' ), 
+    'update_item' => __( 'Update Type' ),
+    'add_new_item' => __( 'Add New Type' ),
+    'new_item_name' => __( 'New Type Name' ),
+    'menu_name' => __( 'Types' ),
+  ); 	
+ 
+  register_taxonomy('types',array('deals'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'type' ),
+  ));
+}

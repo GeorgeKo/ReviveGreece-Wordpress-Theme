ea15d04f9fa6907dccd46619840d2effc53c1e6d
GeorgeKo <16765294+GeorgeKo@users.noreply.github.com>
2019-09-21 16:03:46 +0300

    Add Copyright

--- footer.php
+++ footer.php
@@ -1,22 +1,30 @@
-        
-            </main>
+        </main>
 
+        </div>
+
+        <footer>
+            <div class="footer-container">
+                <h3>Quick Menu</h3>
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'footer-navigation',
+                    'container'      => 'div',
+                    'menu_class'     => 'footer-navigation',
+                    'menu_id'        => 'footer-navigation'
+                ));
+                ?>
             </div>
+            <p class="footer-copyright">
+                &copy; <?php echo date("Y");
+                        echo " ";
+                        echo bloginfo('name'); ?>
+                <?php printf(__('| Developed by %1$s', 'Giko'), '<a href="https//giko.gr" title="GiKo Personal Site"> George Koryllos</a>'); ?>
+            </p>
+        </footer>
 
-            <footer>
-                <div class="footer-container">
-                    <?php
-                        wp_nav_menu( array(
-                            'theme_location' => 'footer-navigation',
-                            'container'      => 'div', 
-                            'menu_class'     => 'footer-navigation', 
-                            'menu_id'        => 'footer-navigation'
-                        ));
-                    ?>
-                </div>
-            </footer>
+        <?php wp_footer(); ?>
+        </div>
+        <!--page-container-->
+        </body>
 
-            <?php wp_footer(); ?>
-        </div> <!--page-container-->
-    </body>
-</html>
+        </html>
